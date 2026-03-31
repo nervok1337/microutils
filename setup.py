62520bea34d1e4f1a022d09d490950076e25d868
Никита <nervok1337@gmail.com>
2026-03-31 12:58:37 +0800

    Added description for Test PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="microutils",
-    version="0.1.0",
+    version="0.1.1",
     description="Simple utility functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    
     install_requires=["requests"],
     python_requires=">=3.10",
 )
